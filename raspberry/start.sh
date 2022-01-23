47789d64133ef66ad9126ef3b45d839fd776d9f1
mickuki321 <michal@zieglerlabs.com>
2022-01-23 11:08:11 +0100

    fix

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "ifconfig wlan0 192.168.1.100"
-echo "cd hexapod"
-echo "serve -s build & npm run:server"
+ifconfig wlan0 192.168.1.100
+cd hexapod
+serve -s build & npm run:server
